1413de379f5b81cdf1f283458ac53786a217c6e5
Fatima Riaz <fatima_gatech@outlook.com>
2025-01-26 16:40:20 -0600

    updating views path

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000 ;
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
